59781e1265632ff3d18e79f2b5e14d3c9ec83a4f
unknown <tareque179@gmail.com>
2021-10-31 21:55:04 +0600

    something message here

--- script.js
+++ script.js
@@ -29,11 +29,39 @@ var swiper = new Swiper(".home-slider", {
     loop : true,
      });
 
+     
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
+
+window.onscroll = () =>{
+
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+
+  section.forEach(sec =>{
+
+    let top = window.scrollY;
+    let height = sec.offsetHeight;
+    let offset = sec.offsetTop - 150;
+    let id = sec.getAttribute('id');
+
+    if(top >= offset && top < offset + height){
+      navLinks.forEach(links =>{
+        links.classList.remove('active');
+        document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+      });
+    };
+
+  });
+
+}
+
+
      var swiper = new Swiper(".review-slider", {
-          spaceBetween: 20,
+          spaceBetween: 2,
           centeredSlides: true,
           autoplay: {
-            delay: 10500,
+            delay: 3500,
             disableOnInteraction: false,
           },
          loop : true,
@@ -52,3 +80,11 @@ var swiper = new Swiper(".home-slider", {
           },
          },
           });
+          function loader(){
+               document.querySelector('.loader-container').classList.add("fade-out");
+          }
+
+          function fadeOut(){
+               setInterval(loader,3000);
+          }
+          window.onload = fadeOut;
